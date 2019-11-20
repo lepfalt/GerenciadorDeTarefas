87f267f8a5f53ecdcd8578fb05a7b9c0a971c0bd
Letícia Pfaltzgraff <lepfalt@gmail.com>
2019-11-20 20:34:29 -0300

    context e mapping usuário.

---
+++ GerenciadorDeTarefas/Infraestrutura/GerenciadorContext.cs
@@ -0,0 +1,38 @@
+﻿using GerenciadorDeTarefas.Infraestrutura.Mappings;
+using GerenciadorDeTarefas.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeTarefas.Infraestrutura
+{
+	public class GerenciadorContext : DbContext
+	{
+		public DbSet<Usuario> Usuarios { get; set; }
+		//public DbSet<Tarefa> Tarefas { get; set; }
+		
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfiguration(new MapUsuario());
+			//modelBuilder.ApplyConfiguration(new MapTarefa());
+
+			base.OnModelCreating(modelBuilder);
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			var config = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json")
+				.Build();
+
+			optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+			optionsBuilder.EnableSensitiveDataLogging();
+		}
+	}
+}
+
---
+++ GerenciadorDeTarefas/Infraestrutura/Mappings/MapUsuario.cs
@@ -0,0 +1,59 @@
+﻿using GerenciadorDeTarefas.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeTarefas.Infraestrutura.Mappings
+{
+	public class MapUsuario : IEntityTypeConfiguration<Usuario>
+	{
+		public void Configure(EntityTypeBuilder<Usuario> builder)
+		{
+			builder
+				.ToTable("tbUsuario");
+
+			builder
+				.Property(u => u.Codigo)
+				.HasColumnName("Codigo")
+				.IsRequired(true);
+
+			builder
+				.HasKey(u => u.Codigo)
+				.HasName("pkUsuario");
+
+			builder
+				.Property(u => u.Nome)
+				.HasColumnName("Nome")
+				.HasColumnType("varchar(80)")
+				.HasMaxLength(80)
+				.IsRequired(true);
+
+			builder
+				.HasIndex(u => u.Nome)
+				.HasName("ixUsuarioNome")
+				.IsUnique(true);
+
+			builder
+				.Property(u => u.Email)
+				.HasColumnName("Email")
+				.HasColumnType("varchar(100)")
+				.HasMaxLength(100)
+				.IsRequired(true);
+
+			builder
+				.Property(u => u.Permissao)
+				.HasColumnName("Permissao")
+				.IsRequired(true);
+
+			builder
+				.Property(u => u.Senha)
+				.HasColumnName("Senha")
+				.HasColumnType("varchar(50)")
+				.HasMaxLength(50)
+				.IsRequired(true);
+		}
+	}
+}
